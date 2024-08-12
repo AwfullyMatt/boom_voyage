395c3d84bdca648ac254712cb4cf36b245e48139
AwfullyMatt <the.xeronite@gmail.com>
2024-08-12 16:27:49 -0600

    fixed attack/sprite spawn

--- src/combat.rs
+++ src/combat.rs
@@ -1,6 +1,6 @@
-use bevy::{prelude::*, window::WindowResized};
+use bevy::{prelude::*, reflect::List, window::WindowResized};
 
-use crate::{AppState, GameState};
+use crate::AppState;
 
 pub struct CombatPlugin;
 
@@ -8,12 +8,15 @@ impl Plugin for CombatPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(Columns::init())
             .insert_resource(Runway::init())
+            .add_event::<EventSpawnAttack>()
             .add_systems(Startup, dummy_system)
-            .add_systems(OnEnter(AppState::Loading), (setup_columns, setup_runway))
+            .add_systems(OnEnter(AppState::Menu), (setup_columns, setup_runway))
             .add_systems(
                 Update,
-                (adjust_columns, adjust_runway).run_if(in_state(GameState::Combat)),
-            );
+                (adjust_columns, adjust_runway, evr_spawn_attack)
+                    .run_if(in_state(AppState::Playing)),
+            )
+            .add_systems(OnEnter(AppState::Playing), evw_spawn_attack);
     }
 }
 
@@ -33,6 +36,7 @@ impl Columns {
 
 #[derive(Clone, Copy, Default)]
 pub struct Column {
+    u: usize,
     x: f32,
     active: bool,
 }
@@ -50,10 +54,13 @@ impl Runway {
 
 #[derive(Component, Clone)]
 pub struct Attack {
-    source: Entity,
-    target: Entity,
-    columns: Vec<Column>,
+    source: Option<Entity>,
+    target: Option<Entity>,
+    columns: Vec<usize>,
 }
+
+#[derive(Event, Clone, Reflect)]
+pub struct EventSpawnAttack(pub Vec<usize>);
 
 fn dummy_system() {}
 
@@ -61,11 +68,12 @@ fn setup_columns(query_window: Query<&Window>, mut columns: ResMut<Columns>) {
     if let Ok(window) = query_window.get_single() {
         let width = window.resolution.width();
         let column_width = width / 5.;
-        columns.first.x = column_width / 2.;
-        columns.second.x = columns.first.x + column_width;
-        columns.third.x = columns.second.x + column_width;
+
+        columns.third.x = 0.;
+        columns.first.x = columns.third.x - (column_width * 2.);
+        columns.second.x = columns.third.x - column_width;
         columns.fourth.x = columns.third.x + column_width;
-        columns.fifth.x = columns.fourth.x + column_width;
+        columns.fifth.x = columns.fourth.x + (column_width * 2.);
 
         info!(
             "[INITIALIZED] Columns:\n
@@ -135,4 +143,65 @@ fn adjust_runway(mut ev_window_resized: EventReader<WindowResized>, mut runway: 
             runway.start, runway.end,
         );
     }
+}
+
+fn evw_spawn_attack(mut evw_spawn_attack: EventWriter<EventSpawnAttack>) {
+    evw_spawn_attack.send(EventSpawnAttack(vec![1, 2, 3, 4]));
+}
+
+fn evr_spawn_attack(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    columns: Res<Columns>,
+    mut ev_spawn_attack: EventReader<EventSpawnAttack>,
+) {
+    for ev in ev_spawn_attack.read() {
+        info!("[EVENT-READ] Spawn Attack");
+        let attack = commands
+            .spawn(Attack {
+                source: None,
+                target: None,
+                columns: ev.0.clone(),
+            })
+            .insert(SpatialBundle {
+                visibility: Visibility::Visible,
+                ..default()
+            })
+            .id();
+
+        for column in &ev.0 {
+            let sprite_bundle = init_column_sprite(*column, &asset_server, &columns);
+            let entity = commands.spawn(sprite_bundle).id();
+            commands.entity(attack).push_children(&[entity]);
+        }
+
+        info!("[SPAWNED] Attack");
+    }
+}
+
+fn init_column_sprite(
+    u: usize,
+    asset_server: &Res<AssetServer>,
+    columns: &Res<Columns>,
+) -> SpriteBundle {
+    let texture = asset_server.load("sprites/box.png");
+    let x = match u {
+        1 => columns.first.x,
+        2 => columns.second.x,
+        3 => columns.third.x,
+        4 => columns.fourth.x,
+        5 => columns.fifth.x,
+        _ => {
+            info!("[ERROR] Unexpected Column ID -- {}", u);
+            0.
+        }
+    };
+
+    let sprite_bundle = SpriteBundle {
+        transform: Transform::from_translation(Vec3::new(x, 0., 0.)),
+        texture,
+        ..default()
+    };
+
+    sprite_bundle
 }
--- src/metronome.rs
+++ src/metronome.rs
@@ -6,11 +6,14 @@ pub struct MetronomePlugin;
 impl Plugin for MetronomePlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(Tempo::init())
+            .insert_resource(CurrentBeat::init())
+            .add_event::<MeasureFinished>()
             .add_systems(Startup, dummy_system)
             .add_systems(OnEnter(AppState::Playing), spawn_metronome)
             .add_systems(
                 Update,
-                (tick_metronome, update_current_beat).run_if(in_state(GameState::Combat)),
+                (tick_metronome, update_current_beat, ev_measure_finished)
+                    .run_if(in_state(AppState::Playing)),
             );
     }
 }
@@ -49,8 +52,17 @@ pub struct Metronome {
     stopwatch: Stopwatch,
 }
 
-#[derive(Resource, Clone, Copy, Deref, DerefMut)]
+#[derive(Resource, Clone, Copy, Deref, DerefMut, Default)]
 pub struct CurrentBeat(pub u8);
+impl CurrentBeat {
+    fn init() -> Self {
+        info!("[INITIALIZED] Current Beat -- 1");
+        CurrentBeat(1)
+    }
+}
+
+#[derive(Event)]
+pub struct MeasureFinished;
 
 fn spawn_metronome(mut commands: Commands) {
     commands.spawn(Metronome::default());
@@ -67,17 +79,25 @@ fn update_current_beat(
     mut query_metronome: Query<&mut Metronome>,
     tempo: Res<Tempo>,
     mut current_beat: ResMut<CurrentBeat>,
+    mut ev_measure_finished: EventWriter<MeasureFinished>,
 ) {
     if let Ok(mut metronome) = query_metronome.get_single_mut() {
         if metronome.stopwatch.elapsed_secs() >= tempo.spb {
             metronome.stopwatch.reset();
             if current_beat.0 >= tempo.ts_top {
-                current_beat.0 = 0;
+                current_beat.0 = 1;
+                ev_measure_finished.send(MeasureFinished);
             } else {
                 current_beat.0 += 1;
             }
             info!("[MODIFIED] Metronome -- Stopwatch Reset");
             info!("[MODIFIED] Current Beat -- {}", current_beat.0);
         }
+    }
+}
+
+fn ev_measure_finished(mut ev_measure_finished: EventReader<MeasureFinished>) {
+    for _ev in ev_measure_finished.read() {
+        info!("[EVENT] Measure Finished.");
     }
 }
